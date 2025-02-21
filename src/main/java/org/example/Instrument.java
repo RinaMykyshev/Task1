a5a92b67694393f2e393767d9048dc8eb62903e4
Ваше Имя <_email@example.com>
2025-02-21 17:41:02 +0500

    First

--- src/main/java/org/example/Instrument.java
+++ src/main/java/org/example/Instrument.java
@@ -1,4 +1,12 @@
 package org.example;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Instrument {
+    public String play(){
+        return "Playing the instrument!";
+    }
+    public String stop(){
+        return "Stopping the instrument!";
+    }
 }
--- src/main/java/org/example/Musician.java
+++ src/main/java/org/example/Musician.java
@@ -1,4 +1,11 @@
 package org.example;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Musician {
+    private final Instrument instrument;
+    @Autowired
+    public Musician(Instrument instrument) {this.instrument = instrument;}
+    public void perform() {System.out.println("Musician is performing: "+instrument.play());}
 }
